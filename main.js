f153e599b170566b28a6cef8cb6a6e7f9eb8d0c2
Andrew Ross <134294344+andycamguy@users.noreply.github.com>
2023-06-01 10:24:17 -0400

    set up the clock function

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 
+function clock(){
+    var d = new Date(); //retrieves the live time
+    var s = d.getSeconds(); // sets the seconds
+    var m = d.getMinutes(); // sets the minute
+    var h = d.getHours(); //sets the hour
+    document.write(h + ":" + m + ":" + s);
+}
+
+setInterval(time,1000); // 1000 millisends equal 1 second. I honestly forgot about that but learned it from Arduino
