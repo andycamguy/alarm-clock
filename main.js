c68f7e1dd9fd215bb3265db4e05f862fab8ed17c
Andrew Ross <134294344+andycamguy@users.noreply.github.com>
2023-06-02 09:45:53 -0400

    Update main.js

--- main.js
+++ main.js
@@ -5,11 +5,11 @@ let time = document.getElementById("currentTime");
 
 // shows the current time on the page
 
-setInterval(() => {
+setInterval(() => { // the function to set the interval as well as create a time string showing the time
     let d = new Date ();
     time.innerHTML = d.toLocaleTimeString();
 },1000)
 
-//I alert for the page 
-setTimeout(function () { alert("You've been staringfor too long"); }, 10000); 
+//I send an alert after so much time
+setTimeout(function () { alert("You've been staring for too long"); }, 10000); 
 //1000 is 1000 ms or a second
