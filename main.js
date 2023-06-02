0ed0c6a567f43bb8fac2b49158649e5a4df0b13f
Andrew Ross <aross9913@gmail.com>
2023-06-02 02:39:50 +0000

    imma dummy

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 
 // gets current time  from html
 
-let time = document.getElementById('currentTime");
+let time = document.getElementById("currentTime");
 
 // shows the current time on the page
 
-setInterval((0 => {
+setInterval(() => {
     let d = new Date ();
-    time.innerHTML = d. toLocaleTimeString();
+    time.innerHTML = d.toLocaleTimeString();
 },1000)
 
 //I alert for the page 
-setTimeout(function () { alert("You've been staringfor too long); }, 10000); //1000 is 1000 ms or a second
+setTimeout(function () { alert("You've been staringfor too long"); }, 10000); 
+//1000 is 1000 ms or a second
